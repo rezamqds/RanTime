bdb31aab3ee9343379470ceaff994aee69b240e5
rezamqds <85675968+rezamqds@users.noreply.github.com>
2023-05-24 21:29:12 +0330

    Update rand.py
    num between 0 1 added rand

--- rand.py
+++ rand.py
@@ -7,7 +7,20 @@ def rand(u=10):
     r = time.monotonic_ns()
     # random less than 10 
     # its easy and have no cond becaz its just ine digit and no need to check it
-    if u<=10:
+
+    if u == 0:
+        return "zero doesnt have any random!"
+
+    if 0 < u < 1:
+        # return (r%10)*0.02
+        while True:
+            r = (time.monotonic_ns()%100) / 10
+            r *= time.monotonic_ns()%10
+            if 0 < r < 1 :
+                return (r)
+                break
+
+    elif u<=10:
         return (r%10)
 
     # for less than 100 must check two digit, it should be less than 100 or equal and also be right before choosen number
@@ -47,11 +60,16 @@ def rand(u=10):
                 return (r)
                 break
 
-# test area ...
-while True:
-    test = rand(64464654)
-    if test <= 64464654:
-        print("Success! --> ", test)
-    else:
-        print("noway!xD")
-        break
+
+print(rand(0.55))
+
+
+
+# # test area ...
+# while True:
+#     test = rand(263)
+#     if test <= 263:
+#         print("Success! --> ", test)
+#     else:
+#         print("noway!xD")
+#         break
