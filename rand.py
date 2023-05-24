6cdde49937adf928c643752e58654ac9b74bbc1d
rezamqds <85675968+rezamqds@users.noreply.github.com>
2023-05-24 20:33:59 +0330

    Update rand.py
    add two digits random

--- rand.py
+++ rand.py
@@ -8,13 +8,30 @@ import time, numpy
 
 
 def rand(u=10):
+    #random choose
     r = time.monotonic_ns()
+    # random less than 10 
+    # its easy and have no cond becaz its just ine digit and no need to check it
     if u<=10:
-        print(r%10)
+        return (r%10)
+
+    # for less than 100 must check two digit, it should be less than 100 or equal and also be right before choosen number
     elif u<=100:
-        print(r%100)
+        while True:
+            r = time.monotonic_ns()%100
+            if r <= u :
+                return (r)
+                break
+
+
 
         
 
 
-rand()
+while True:
+    test = rand(73)
+    if test <= 73:
+        print("Success! --> ", test)
+    else:
+        print("noway!xD")
+        break
