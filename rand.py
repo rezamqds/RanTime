4b6a335485f9219eabbb80bdb86e8c17eacdec3a
rezamqds <85675968+rezamqds@users.noreply.github.com>
2023-05-24 22:11:02 +0330

    Update rand.py
    fix problems and make the code more effective

--- rand.py
+++ rand.py
@@ -1,17 +1,19 @@
 import time
-
-
-
+# no numpuy or 
 def rand(u=10):
     #random choose
-    r = time.monotonic_ns()
+    # r = time.monotonic_ns()
     # random less than 10 
     # its easy and have no cond becaz its just ine digit and no need to check it
 
     if u == 0:
         return "zero doesnt have any random!"
 
-    if 0 < u < 1:
+    elif u < 0 :
+        res = rand(-u)
+        return -res
+
+    elif 0 < u <= 1:
         # return (r%10)*0.02
         while True:
             r = (time.monotonic_ns()%100) / 10
@@ -21,7 +23,11 @@ def rand(u=10):
                 break
 
     elif u<=10:
-        return (r%10)
+        while True:
+            r = time.monotonic_ns()%100
+            if r <= u :
+                return (r)
+                break
 
     # for less than 100 must check two digit, it should be less than 100 or equal and also be right before choosen number
     elif u<=100:
@@ -56,20 +62,16 @@ def rand(u=10):
         # Dlen: use it for times that should mul the num in 10
         while True:
             r = time.monotonic_ns()%Dlen
+            print(f"its not the res: {r}")
             if r <= u :
                 return (r)
                 break
 
 
-print(rand(0.55))
-
-
-
-# # test area ...
-# while True:
-#     test = rand(263)
-#     if test <= 263:
-#         print("Success! --> ", test)
-#     else:
-#         print("noway!xD")
-#         break
+# user input :
+while True:
+    try:
+        randinput = float(input("Pls input max rand: "))
+        print("\t\t\trand : ",rand(randinput),"\n")
+    except:
+        print("if you wanna close me use Alt+F4 xD\n")
